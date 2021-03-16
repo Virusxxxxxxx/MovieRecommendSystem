e98ec9cb39fa80769963e3433d6e658e6655ee1f
HXX <1476374914@qq.com>
2021-03-16 23:17:44 +0800

    joint debugging 1

--- recommender/KafkaStream/src/main/java/cn/hxx/kafkastream/Application.java
+++ recommender/KafkaStream/src/main/java/cn/hxx/kafkastream/Application.java
@@ -1,4 +1,42 @@
 package cn.hxx.kafkastream;
 
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.processor.TopologyBuilder;
+
+import java.util.Properties;
+// TODO 这里是直接粘贴的
 public class Application {
+    public static void main(String[] args) {
+        String brokers = "myCent:9092";
+        String zookeepers = "myCent:2181";
+
+        // 输入和输出的topic
+        String from = "log";
+        String to = "recommender";
+
+        // 定义kafka streaming的配置
+        Properties settings = new Properties();
+        settings.put(StreamsConfig.APPLICATION_ID_CONFIG, "logFilter");
+        settings.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, brokers);
+        settings.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, zookeepers);
+
+        // 创建 kafka stream 配置对象
+        StreamsConfig config = new StreamsConfig(settings);
+
+        // 创建一个拓扑建构器
+        TopologyBuilder builder = new TopologyBuilder();
+
+        // 定义流处理的拓扑结构
+        builder.addSource("SOURCE", from)
+                .addProcessor("PROCESSOR", ()->new LogProcessor(), "SOURCE")
+                .addSink("SINK", to, "PROCESSOR");
+
+        KafkaStreams streams = new KafkaStreams( builder, config );
+
+        streams.start();
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>> Kafka stream started! >>>>>>>>>>>>>>>>>>>>>>>");
+
+    }
 }
