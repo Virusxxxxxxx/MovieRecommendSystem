23ae68d0655ea151e36a6275aeb71cbfe2563f66
HXX <1476374914@qq.com>
2021-03-30 21:59:10 +0800

    ContentRecommender2 finished

--- recommender/ContentRecommender/src/main/scala/cn/hxx/ContentRecommender2/ContentRecommender2.scala
+++ recommender/ContentRecommender/src/main/scala/cn/hxx/ContentRecommender2/ContentRecommender2.scala
@@ -58,7 +58,7 @@ object ContentRecommender2 {
         //jieba -> TF-IDF -> word2vec -> cos
 
         //从Comment表中加载数据
-        val commentRow = spark.read
+        val commentRaw = spark.read
                 .option("uri", mongoConfig.uri)
                 .option("collection", MONGODB_COMMENT_COLLECTION)
                 .format("com.mongodb.spark.sql")
@@ -91,7 +91,7 @@ object ContentRecommender2 {
         val broadcast_StopWords = sc.broadcast(sc.textFile(STOP_WORD_PATH).collect().toSet)
 
         //利用jieba + word2vec处理评论
-        val commentDF = commentRow.map{
+        val commentDF = commentRaw.map{
             case (uid, mid, content) =>
                 val dbc_comment = AsciiUtil.sbc2dbcCase(content) //全角转半角
                 val participle_comment = participleFunc(dbc_comment) //分词 => 我 爱 刘德华
@@ -108,7 +108,7 @@ object ContentRecommender2 {
                 }
                 similar_list ++= participle_comment     //保留原始分词
                 (uid, mid, similar_list.mkString(" "))
-        }.toDF("uid", "mid", "content")
+        }.filter(!_._3.isEmpty).toDF("uid", "mid", "content")   //过滤空值，并指定列名
 
         // 用TF-IDF从内容信息中提取电影特征向量
         // step1: 创建一个分词器，默认按空格分词
@@ -137,90 +137,108 @@ object ContentRecommender2 {
         // => (50,[26,27,36],[2.194720551703029,0.7605551441254693,1.7079767945947977])
         val rescaledData = idfModel.transform(featurizedData)
 
-        rescaledData.show(5,truncate = false)
+//        rescaledData.show(5,truncate = false)
 
-//        val userFeatures = rescaledData.map(
-//            // 这里目标格式是(mid, features), 即(Int, Array[Double]) (参照OfflineRecommender)
-//            // 对SparseVector稀疏矩阵类型使用toArray，可以转换为正常数组 => (0.0,...,1.0,0.0,...,0.0)
-//            row => (row.getAs[Int]("uid"), row.getAs[SparseVector]("features").toArray)
-//        ).rdd.map( //转成rdd (mllib里的类型都是rdd, 而ml里的类型是DataFrame)
-//            x => (x._1, new DoubleMatrix(x._2))
-//        )
-//
+        val userFeatures = rescaledData.map(
+            // 这里目标格式是(uid, features), 即(Int, Array[Double]) (参照OfflineRecommender)
+            // 对SparseVector稀疏矩阵类型使用toArray，可以转换为正常数组 => (0.0,...,1.0,0.0,...,0.0)
+            row => (row.getAs[Int]("uid"), row.getAs[SparseVector]("features").toArray)
+        ).rdd.map( //转成rdd (mllib里的类型都是rdd, 而ml里的类型是DataFrame)
+            x => (x._1, new DoubleMatrix(x._2))
+        )
+
 //        userFeatures.collect().take(10).foreach(println)
 
 //***************************************************************************************************************
 
-//        //从Movie表中加载数据，并预处理
-//        val movieTagsDF = spark.read
-//                .option("uri", mongoConfig.uri)
-//                .option("collection", MONGODB_MOVIE_COLLECTION)
-//                .format("com.mongodb.spark.sql")
-//                .load()
-//                .limit(5000)
-//                .as[Movie]
-//                .map(
-//                    x => (x.mid, x.name, x.genres.map(g => if (g == '/') ' ' else g), x.descri)
-//                )
-//                .toDF("mid", "name", "genres", "describe") //指定列名
-//
-//        val tokenizer1 = new Tokenizer().setInputCol("genres").setOutputCol("words")
-//        // 用分词器对原始数据做转换，生成新的一列words
-//        // [Drama Horror Thriller] => [drama, horror, thriller]
-//        val wordsData1 = tokenizer1.transform(movieTagsDF)
-//
-//        // step2: 引入HashingTF工具
-//        // 可以把一个词语序列转换成对应的词频
-//        // [drama, horror, thriller] => (50,[26,27,36],[1.0,1.0,1.0])
-//        // 相当于hash散列，50是设定的hash表长度，表示这三个类别散列到在26 27 36的位置上，值为1
-//        // 其类型是SparseVector(稀疏矩阵), 并且用三元组表示[hash表长, 数据位置, 数据]
-//        val hashingTF1 = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(50)
-//        val featurizedData1 = hashingTF1.transform(wordsData1)
-//
-//        //        featurizedData.show(truncate = false)
-//
-//        // step3: 引入IDF工具
-//        val idf1 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-//        // 训练IDF模型
-//        val idfModel1 = idf1.fit(featurizedData1)
-//        // 用模型对rawFeatures进行处理，得到每个词的TF-IDF，作为新的特征向量
-//        // => (50,[26,27,36],[2.194720551703029,0.7605551441254693,1.7079767945947977])
-//        val rescaledData1 = idfModel1.transform(featurizedData1)
-//
-//        //        rescaledData.show(truncate = false)
-//
-//        val movieFeatures = rescaledData1.map(
-//            // 这里目标格式是(mid, features), 即(Int, Array[Double]) (参照OfflineRecommender)
-//            // 对SparseVector稀疏矩阵类型使用toArray，可以转换为正常数组 => (0.0,...,1.0,0.0,...,0.0)
-//            row => (row.getAs[Int]("mid"), row.getAs[SparseVector]("features").toArray)
-//        ).rdd.map(  //转成rdd (mllib里的类型都是rdd, 而ml里的类型是DataFrame)
-//            x => (x._1, new DoubleMatrix(x._2))
-//        )
-//
-//        // (后面全是复制Offline里的)对所有电影两两计算相似度
-//        // 这里的movieFeatures相当于是把标签hash散列后再TD-IDF的标签权值作为特征
-//        // 而Offline里的movieFeatures则是通过隐语义模型得到的隐特征
-//        val movieRecs = userFeatures.cartesian(movieFeatures) //笛卡尔积
-//                .map {
-//                    case (a, b) => {
-//                        //计算用户和电影的余弦相似度
-//                        val simScore = this.consinSim(a._2, b._2)
-//                        (a._1, (b._1, simScore)) //(user, (movie, simScore))
-//                    }
-//                }
-//                //这里filter可以放到SteamingRecommender中的computeMovieScore函数中来过滤simScore
-//                .filter(_._2._2 > 0.6) //过滤出相似度评分大于0.6的
-//                .groupByKey() //(user, ((movieB, score),(movieC, score),...)
-//                .map { //封装成MovieRecs
-//                    case (mid, items) =>
-//                        MovieRecs(
-//                            mid,
-//                            items.toList.sortWith(_._2 > _._2).map(x => Recommendation(x._1, x._2))
-//                        )
-//
-//                }
-//                .toDF()
-//        storeRecsInMongoDB(movieRecs, CONTENT_MOVIE_RECS)
+        //从Movie表中加载数据，并预处理
+        val movieRaw = spark.read
+                .option("uri", mongoConfig.uri)
+                .option("collection", MONGODB_MOVIE_COLLECTION)
+                .format("com.mongodb.spark.sql")
+                .load()
+                .limit(5000)
+                .as[Movie]
+                .map(
+                    x => (x.mid, x.name, x.genres.map(g => if (g == '/') ' ' else g), x.descri)
+                )
+
+        val movieTagsDF = movieRaw.map{
+            case (mid, name, genres, describe) =>
+                val dbc_describe = AsciiUtil.sbc2dbcCase(describe) //全角转半角
+                val participle_describe = participleFunc(dbc_describe) //对电影简介分词
+                        .split(" ")
+                        .filter(
+                            x => !broadcast_StopWords.value.contains(x) //过滤停用词
+                                    && key_list.contains(x)) //判断是否在word2vec词典内
+                val similar_list = mutable.HashSet[String]() //用HashSet去重
+                for (item <- participle_describe) { //遍历刚才得到的分词
+                    val like = word2vec.findSynonyms(item, 2) //找相近的两个词
+                    for ((item, cos) <- like) {
+                        similar_list += item
+                    }
+                }
+                similar_list ++= participle_describe     //保留原始分词
+                (mid, similar_list.mkString(" "))
+        }.toDF("mid", "describe").cache()
+
+        val tokenizer1 = new Tokenizer().setInputCol("describe").setOutputCol("words")
+        // 用分词器对原始数据做转换，生成新的一列words
+        // [Drama Horror Thriller] => [drama, horror, thriller]
+        val wordsData1 = tokenizer1.transform(movieTagsDF)
+
+        // step2: 引入HashingTF工具
+        // 可以把一个词语序列转换成对应的词频
+        // [drama, horror, thriller] => (50,[26,27,36],[1.0,1.0,1.0])
+        // 相当于hash散列，50是设定的hash表长度，表示这三个类别散列到在26 27 36的位置上，值为1
+        // 其类型是SparseVector(稀疏矩阵), 并且用三元组表示[hash表长, 数据位置, 数据]
+        val hashingTF1 = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(100)
+        val featurizedData1 = hashingTF1.transform(wordsData1)
+
+        //        featurizedData1.show(truncate = false)
+
+        // step3: 引入IDF工具
+        val idf1 = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+        // 训练IDF模型
+        val idfModel1 = idf1.fit(featurizedData1)
+        // 用模型对rawFeatures进行处理，得到每个词的TF-IDF，作为新的特征向量
+        // => (50,[26,27,36],[2.194720551703029,0.7605551441254693,1.7079767945947977])
+        val rescaledData1 = idfModel1.transform(featurizedData1)
+
+//        rescaledData1.show(truncate = false)
+
+        val movieFeatures = rescaledData1.map(
+            // 这里目标格式是(mid, features), 即(Int, Array[Double]) (参照OfflineRecommender)
+            // 对SparseVector稀疏矩阵类型使用toArray，可以转换为正常数组 => (0.0,...,1.0,0.0,...,0.0)
+            row => (row.getAs[Int]("mid"), row.getAs[SparseVector]("features").toArray)
+        ).rdd.map(  //转成rdd (mllib里的类型都是rdd, 而ml里的类型是DataFrame)
+            x => (x._1, new DoubleMatrix(x._2))
+        )
+
+        // (后面全是复制Offline里的)对电影和用户的vector两两计算相似度
+        // 这里的movieFeatures相当于是把标签hash散列后再TD-IDF的标签权值作为特征
+        // 而Offline里的movieFeatures则是通过隐语义模型得到的隐特征
+        val movieRecs = userFeatures.cartesian(movieFeatures) //笛卡尔积
+                .map {
+                    case (a, b) => {
+                        //计算用户和电影的余弦相似度
+                        val simScore = this.consinSim(a._2, b._2)
+                        (a._1, (b._1, simScore)) //(user, (movie, simScore))
+                    }
+                }
+                //这里filter可以放到SteamingRecommender中的computeMovieScore函数中来过滤simScore
+                .filter(_._2._2 > 0.6) //过滤出相似度评分大于0.6的
+                .groupByKey() //(user, ((movieB, score),(movieC, score),...)
+                .map { //封装成MovieRecs
+                    case (mid, items) =>
+                        MovieRecs(
+                            mid,
+                            items.toList.sortWith(_._2 > _._2).map(x => Recommendation(x._1, x._2))
+                        )
+
+                }
+                .toDF()
+        storeRecsInMongoDB(movieRecs, CONTENT_MOVIE_RECS)
 
         spark.close()
     }
